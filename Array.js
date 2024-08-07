83cfd494ae8870f444ff13b973b8c6430da402e0
Saud <123446988+Saudsaad5@users.noreply.github.com>
2024-08-07 13:16:23 +0300

    Array in java script

---
+++ Array.js
@@ -0,0 +1,8 @@
+var guestList = ["saud", "eyad", "suhib", "abdullah", "hamoud"];
+var inList = prompt("Enter your name: ");
+
+if (guestList.includes(inList)){
+    alert("You are welcom !!");
+}else{
+    alert("You are not invited !!");
+}
